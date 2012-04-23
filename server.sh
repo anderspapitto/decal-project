0f9252e52472846dbacf4dd15787eea09e5d8288
Anders Papitto <anderspapitto@gmail.com>
2012-04-23 16:12:41 -0700

    removed reference to aber directory

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ if [ ! $DECAL ] ; then # first time script is executed in a session
     export DECALPORT=10002 # further connections will start here
 fi
 kill `lsof -i :$DECALPORT | tail -n +2 | sed -e 's,[ \t][ \t]*, ,g' | cut -f2 -d' '` 2>/dev/null # clears junk on DECALPORT
-\rm ~/aber/pipe 2>/dev/null; mknod ~/aber/pipe p && nc -l $DECALPORT 0<~/aber/pipe | ./execute-commands.sh 1>~/aber/pipe & # space for one user connection
+\rm ./pipe 2>/dev/null; mknod ./pipe p && nc -l $DECALPORT 0<./pipe | ./execute-commands.sh 1>./pipe & # space for one user connection
 kill `lsof -i :10001 | tail -n +2 | sed -e 's,[ \t][ \t]*, ,g' | cut -f2 -d' '` 2>/dev/null # clears junk on port 10001
 clientresp=`echo $DECALPORT | nc -l 10001` #wait for more users
 export DECALPORT=$(expr $DECALPORT + 1) # further users will go on higher ports
